3655747825cc156a0236cec00f1b1bbce038c7c1
rezimOne <michalreda@gmail.com>
2020-11-01 20:41:58 +0100

    feature(components, routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { AboutComponent } from './views/about/about.component';
+import { SearchComponent } from './views/search/search.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, SearchComponent, AboutComponent, PageNotFoundComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class ViewsModule { }
