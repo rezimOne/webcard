5326eb909b83743f1f74945d1b3141bc88e8cabf
rezimOne <michalreda@gmail.com>
2020-11-01 19:51:19 +0100

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { ViewsModule } from './views/views.module';
+import { CoreModule } from './core/core.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    ViewsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
